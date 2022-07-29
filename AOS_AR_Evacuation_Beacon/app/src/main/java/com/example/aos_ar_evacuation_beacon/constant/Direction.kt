4404d1bd5720f6c56321c358dc74af560a9f844c
Jiwon <senta2006@naver.com>
2022-07-29 08:56:24 -0400

    fix: add Direction, Floor enums

---
+++ AOS_AR_Evacuation_Beacon/app/src/main/java/com/example/aos_ar_evacuation_beacon/constant/Direction.kt
@@ -0,0 +1,5 @@
+package com.example.aos_ar_evacuation_beacon.constant
+
+enum class Direction {
+   North, East, South, West
+}
---
+++ AOS_AR_Evacuation_Beacon/app/src/main/java/com/example/aos_ar_evacuation_beacon/constant/Floor.kt
@@ -0,0 +1,5 @@
+package com.example.aos_ar_evacuation_beacon.constant
+
+enum class Floor {
+   First, Second, Base
+}
