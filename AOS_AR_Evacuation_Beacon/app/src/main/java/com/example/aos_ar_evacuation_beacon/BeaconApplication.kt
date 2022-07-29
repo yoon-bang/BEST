c241828925a2ec2d50c1120c4139fed44870bf1a
Jiwon <senta2006@naver.com>
2022-07-29 08:59:44 -0400

    fix: formatting BeaconApplication.kt

--- AOS_AR_Evacuation_Beacon/app/src/main/java/com/example/aos_ar_evacuation_beacon/BeaconApplication.kt
+++ AOS_AR_Evacuation_Beacon/app/src/main/java/com/example/aos_ar_evacuation_beacon/BeaconApplication.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.Observer
+import com.example.aos_ar_evacuation_beacon.ui.LocalizationActivity
 import org.altbeacon.beacon.*
 
 class BeaconApplication : Application() {
@@ -20,21 +21,17 @@ class BeaconApplication : Application() {
       beaconManager.beaconParsers.clear()
 
       // The example shows how to find iBeacon.
-      beaconManager.beaconParsers.add(
-         BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24")
-                                     )
+      beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
 
       //setupForegroundService()
       //beaconManager.setEnableScheduledScanJobs(false);
       //beaconManager.setBackgroundBetweenScanPeriod(0);
-      beaconManager.backgroundScanPeriod = 1100;
-      beaconManager.foregroundScanPeriod = 1100;
+      beaconManager.backgroundScanPeriod = 2000;
+      beaconManager.foregroundScanPeriod = 2000;
 
 
       // UUID 설정
-      region = Region(
-         "all-beacons", Identifier.parse("$beaconID"), null, null
-                     )
+      region = Region("all-beacons", Identifier.parse("$beaconID"), null, null)
       beaconManager.startMonitoring(region)
       beaconManager.startRangingBeacons(region)
       val regionViewModel = BeaconManager.getInstanceForApplication(this).getRegionViewModel(region)
@@ -47,13 +44,9 @@ class BeaconApplication : Application() {
       builder.setSmallIcon(R.drawable.ic_launcher_background)
       builder.setContentTitle("Scanning for Beacons")
       val intent = Intent(this, LocalizationActivity::class.java)
-      val pendingIntent = PendingIntent.getActivity(
-         this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT + PendingIntent.FLAG_IMMUTABLE
-                                                   )
+      val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT + PendingIntent.FLAG_IMMUTABLE)
       builder.setContentIntent(pendingIntent);
-      val channel = NotificationChannel(
-         "beacon-ref-notification-id", "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT
-                                       )
+      val channel = NotificationChannel("beacon-ref-notification-id", "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT)
       channel.description = "My Notification Channel Description"
       val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
       notificationManager.createNotificationChannel(channel);
@@ -85,9 +78,7 @@ class BeaconApplication : Application() {
             .setSmallIcon(R.drawable.ic_launcher_background)
       val stackBuilder = TaskStackBuilder.create(this)
       stackBuilder.addNextIntent(Intent(this, LocalizationActivity::class.java))
-      val resultPendingIntent = stackBuilder.getPendingIntent(
-         0, PendingIntent.FLAG_UPDATE_CURRENT + PendingIntent.FLAG_IMMUTABLE
-                                                             )
+      val resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT + PendingIntent.FLAG_IMMUTABLE)
       builder.setContentIntent(resultPendingIntent)
       val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
       notificationManager.notify(1, builder.build())
--- AOS_AR_Evacuation_Beacon/app/src/main/java/com/example/aos_ar_evacuation_beacon/MyFirebaseMessagingService.kt
+++ AOS_AR_Evacuation_Beacon/app/src/main/java/com/example/aos_ar_evacuation_beacon/MyFirebaseMessagingService.kt
@@ -9,6 +9,7 @@ import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.example.aos_ar_evacuation_beacon.ui.LocalizationActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
