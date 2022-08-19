8b946bb92bfd23e22bb0c88aa9e8f3c35a306f0e
Jiwon <senta2006@naver.com>
2022-08-19 18:06:55 +0900

    방위각 기반 알고리즘 추가

--- AOS_AR_Evacuation_Beacon/app/src/main/java/com/example/aos_ar_evacuation_beacon/constant/Direction.kt
+++ AOS_AR_Evacuation_Beacon/app/src/main/java/com/example/aos_ar_evacuation_beacon/constant/Direction.kt
@@ -1,5 +1,5 @@
 package com.example.aos_ar_evacuation_beacon.constant
 
-enum class Direction {
-   North, East, South, West
+enum class Direction(val index: Int) {
+   South(0), East(1), North(2), West(3)
 }
